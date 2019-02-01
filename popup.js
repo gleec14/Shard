1a8b4e165d5f3e84a8258a6c8696afc44a186ce2
Gregory Lee <gleec14@gmail.com>
2019-02-01 17:25:54 -0500

    Integrate project with JQuery

--- popup.js
+++ popup.js
@@ -5,20 +5,42 @@
 
 // Adds all tabs in the current window to a new Bookmarks folder
 function addNewGem() {
-
+  alert("addNewGem");
 }
 
 // Loads all tabs in a chosen Bookmarks folder in a new window
 function loadGem() {
-
+  alert("loadGem");
 }
 
 // Display all Bookmark folders
 function displayGems() {
-
+  alert("displayGems")
 }
 
+// Removes selected Gems
+function removeGems() {
+  alert("removeGems")
+}
+
+// Creates Gem card
+function createCard() {
+  let card =
+    `<div class="card">
+      <h4> Gem </h4>
+    </div>`;
+  let cards = '';
+  let amount = 5;
+  for(let i = 0; i < amount; i++){
+    cards += card;
+  }
+  $(".gems").empty().append(cards);
+}
+
+//document.addEventListener('DOMContentLoaded')
 //Add listeners to buttons
-document.getElementById("addNewGem").addEventListener("click", createNewGem);
-document.getElementById("loadGem").addEventListener("click", loadGem);
-document.getElementById("displayGems").addEventListener("click", displayGems);
+$("addNewGem").click(addNewGem);
+$("loadGem").click(loadGem);
+$(document).ready(createCard)
+//document.getElementById("displayGems").addEventListener("click", displayGems);
+//document.getElementById("removeGems").addEventListener("click", removeGems);
