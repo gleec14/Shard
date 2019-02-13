c4c9b435684e30248bee0730b694299ec9aa719d
unknown <gleec14@gmail.com>
2019-02-13 16:32:25 +0000

    gems load tabs in new window

--- popup.js
+++ popup.js
@@ -9,14 +9,41 @@ function addNewGem() {
 }
 
 // Loads all tabs in a chosen Bookmarks folder in a new window
-function loadGem() {
-  alert("loadGem");
+function loadGems(bookmarkNode, folderName) {
+  var folders = getFolders(bookmarkNode);
+  for (var i = 0; i < folders.length; i++) {
+    if (folders[i].title == folderName) {
+       var bookmarks = folders[i].children;
+       var urls = [];
+       for (var j = 0; j < bookmarks.length; j++) {
+         urls.push(bookmarks[j].url);
+       }
+       chrome.windows.create({'url': urls}, function(windows){});
+       break;
+    }
+  }
 }
 
-// Display all Bookmark folders
-//function displayGems() {
-//  alert("displayGems")
-//}
+function loadGemsTest(folders, folderName) {
+  for (var i = 0; i < folders.length; i++){
+    if (folders[i].title == folderName) {
+       var bookmarks = folders[i].children;
+       var urls = [];
+       for (var j = 0; j < bookmarks.length; j++) {
+         urls.push(bookmarks[j].url);
+       }
+       chrome.windows.create({'url': urls}, function(windows){});
+       break;
+    }
+  }
+}
+
+function addListenerToGems(bookmarkNode) {
+  var folders = getFolders(bookmarkNode);
+  for (var i = 0; i < folders.length; i++) {
+    $(document).on("click",'.gem',loadGems(bookmarkNode, folders[i].title));
+  }
+}
 
 // Removes selected Gems
 function removeGems() {
@@ -25,9 +52,9 @@ function removeGems() {
 
 // Traverse the bookmark tree, and print folders
 function displayGems(){
-  var bookmarkNodes = chrome.bookmarks.getTree(
+  var bn = chrome.bookmarks.getTree(
     function(bookmarkNodes) {
-      $('.gems').append(createPanels(bookmarkNodes));
+      createPanels(bookmarkNodes);
     });
 }
 
@@ -35,24 +62,31 @@ function displayGems(){
 function createPanels(bookmarkNodes) {
   var panels = '';
   var bookmarks;
-  var i;
-  for (i = 0; i < bookmarkNodes.length; i++) {
-    var folders = getFolders(bookmarkNodes[i]);
-    for (var j = 0; j < folders.length; j++) {
-      var panel = '<div class="gem"><h4>'
-                   + folders[j].title
-                    + '</h4></div>';
-      panels += panel;
-    }
+  var folders = getFolders(bookmarkNodes[0]);
+  for (var j = 0; j < folders.length; j++) {
+    var panel = '<div class="gem" id="gem' + j + '"><h4>'
+                 + folders[j].title
+                  + '</h4></div>';
+    $('.gems').append(panel);
+    $('.gems').on('click','#gem' + j, function(event){
+           var gem_num = event.handleObj.selector;
+           gem_num = gem_num.slice(4);
+           gem_num = parseInt(gem_num);
+           var bookmarks = folders[gem_num].children;
+           var urls = [];
+           for (var k = 0; k < bookmarks.length; k++) {
+             urls.push(bookmarks[k].url);
+           }
+           chrome.windows.create({'url': urls}, function(windows){});
+    });
+    panels += panel;
   }
-  bglog(panels);
-  return panels;
 }
 
 function getFolders(bookmarkNode) {
-  bglog(bookmarkNode.children[1].children);
-  // gets the bookmarks in "Other bookmarks"
-  return bookmarkNode.children[1].children;
+  // gets the folders in "Other bookmarks"
+  var folders = bookmarkNode.children[1].children;
+  return folders;
 }
 
 // console.log in the background page
@@ -62,7 +96,8 @@ var bglog = function(obj) {
 	}
 }
 
-$(document).ready(displayGems);
+$(document).ready(function() {
+  displayGems();
+});
 //Add listeners to buttons
 $("#addNewGem").click(addNewGem);
-$("#loadGem").click(loadGem);
